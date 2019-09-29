9a44f738b64a3d7d9735b3f577e20f04b3fd655e
Kyle Ruff <kyleruff@gmail.com>
2019-09-28 20:00:34 -0600

    initial heroku commit

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => (
+  <div className="container">
+    <div className="jumbotron bg text-center">
+      <h1 className="logo">Click-Out!!</h1>
+      <p className="desc">Click on a death metal band logo to earn points, if you click the same band twice and lose!</p>
+    </div>
+  </div>
+);
+
+export default Header;
